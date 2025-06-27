b7ea489405ceb1e0a370c3618046786d016fd292
PointBlanc25 <nattra70@kingshigh.school.nz>
2025-06-27 13:16:35 +1200

    Got the GUI to display items, but it's quite broken currently

---
+++ Scenes/ui.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+@export var description : NinePatchRect
+
+func set_description(item : Item):
+	description.find_child("Name").text = item.title
+	description.find_child("Icon").texture = item.icon
+	description.find_child("Description").text = item.description
---
+++ slot.gd
@@ -0,0 +1,10 @@
+extends PanelContainer
+
+@export var item : Item:
+	set (value):
+		item = value
+		$Icon.texture = item.icon
+
+func _on_mouse_entered() -> void:
+	if item != null:
+		owner.set_description(item)

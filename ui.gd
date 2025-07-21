45165e0d6823f619e95eeefa7aeab9cf9a35fffa
PointBlanc25 <nattra70@kingshigh.school.nz>
2025-07-21 13:07:11 +1200

    Fix Broken Thing

--- ui.gd
+++ ui.gd
@@ -5,5 +5,5 @@ extends Control
 
 func set_description(item : Item):
 	description.find_child("Name").text = item.title
-	description.find_child("Icon").texture = item.icon
+	description.find_child("PreviewIcon").texture = item.icon
 	description.find_child("Description").text = item.description
